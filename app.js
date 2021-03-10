152925ca54f89549972a4b5523f650436f0ac948
filecage <mail@dbeuchert.com>
2021-03-10 01:58:36 +0100

    adds prototype app

---
+++ app.js
@@ -0,0 +1,25 @@
+import webpackConfig from './app/webpack-local.js';
+import {createFsFromVolume, Volume} from 'memfs';
+import {collectComponentDocs} from './src/server/Helper/collector.js';
+import {parse} from './src/server/Helper/parser.js';
+import http from './src/server/Http/http.js';
+import Compiler from './src/server/Compiler/Compiler.js';
+import JSXCompiler from './src/server/Compiler/JSX/JSXCompiler.js';
+import assemble from './src/server/Helper/assemble.js';
+import Doc from './src/server/Doc.js';
+import DocContainer from './src/server/DocContainer.js';
+
+const files = await collectComponentDocs('./app');
+const docs = [];
+for (const file of files) {
+    const fs = createFsFromVolume(new Volume());
+    const component = await parse(file);
+    const compiler = new Compiler(fs, new JSXCompiler('./', webpackConfig));
+
+    fs.writeFileSync('/index.html', await assemble(compiler, component));
+
+    docs.push(new Doc(component, fs));
+}
+
+http(new DocContainer(...docs));
+
